97bb9e696271aa996adefe12e8eaeaa9d20351bb
Deepkumar Patel <deepkumarpatel471@gmail.com>
2026-05-19 10:10:33 +0530

    update types

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -4,6 +4,6 @@ mod series;
 mod tags;
 
 pub use query::{Query, TagFilter};
-pub use sample::Sample;
+pub use sample::{Sample, SampleReading, SeriesKey};
 pub use series::Series;
 pub use tags::Tags;
--- src/types/sample.rs
+++ src/types/sample.rs
@@ -2,8 +2,18 @@ use crate::types::Tags;
 
 #[derive(Debug, Clone)]
 pub struct Sample {
+    pub key: SeriesKey,
+    pub reading: SampleReading,
+}
+
+#[derive(Debug, Clone)]
+pub struct SeriesKey {
     pub metric: String,
     pub tags: Tags,
+}
+
+#[derive(Debug, Clone)]
+pub struct SampleReading {
     pub timestamp: i64,
     pub value: f64,
 }
